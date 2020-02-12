ea4c97ed77815d995e626fd629977ab2394bbecf
OpLpEpG <ooorivas@gmail.com>
2020-02-12 08:33:04 +0500

    ggg

--- tst.py
+++ tst.py
@@ -3,4 +3,36 @@ pyarr = [1, 2, 3, 4]
 arr = (ctypes.c_uint16 * len(pyarr))(*pyarr)
 print(*pyarr, bytes(arr))
 
-class FromCount
+import asyncio, asyncio.futures
+
+async def ares():
+    return 42
+
+
+class Awaitable(asyncio.Future):
+    def __await__(self):
+        if not self.done():
+            print(self._state)
+            self.set_result(10)
+            print(self._state)
+#        for _ in range(80):   
+#            print('.',end='') 
+#            yield from self
+        return super().__await__()    
+        
+
+async def func():
+    e = await Awaitable()
+    print('e', e)
+    return e+1
+
+async def main():
+    print('main1')
+    print(await func())  
+    print('main2')
+
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
